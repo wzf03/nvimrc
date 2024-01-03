aa4b028fe6b2e0a58e213a28f460fe2b122fed5b
Zhuofeng Wang <zhuofengwang2003@gmail.com>
2024-01-03 18:59:13 +0800

    Update config for im-select plugins.

---
+++ lua/plugins/imselect.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    {
+      "wzf03/im-select.nvim",
+      opts = {
+        set_default_events = { "VimEnter", "InsertLeave" },
+        set_previous_events = { "VimLeave", "InsertEnter" },
+      },
+    },
+  },
+}
