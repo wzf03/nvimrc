302b492a6df95e171239e61801ca8d4de84a60ac
Zhuofeng Wang <zhuofengwang2003@gmail.com>
2024-01-03 18:00:13 +0800

    Update keymap for paste and yank

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- solve problems on Redmibook Pro 15
+map("", "<kEnter>", "<Enter>", {})
+
+-- yank and paste from system clipboard
+map("v", "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+map("n", "<leader>p", '"+p"', { desc = "Paste from system clipboard" })
+map("n", "<leader>P", '"+P"', { desc = "Paste from system clipboard" })
