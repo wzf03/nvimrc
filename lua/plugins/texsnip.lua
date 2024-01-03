67f6c574ccd0a535820e5b541b428a9483535c43
Zhuofeng Wang <zhuofengwang2003@gmail.com>
2024-01-03 20:41:41 +0800

    Update some plugins configurations.

---
+++ lua/plugins/texsnip.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "iurimateus/luasnip-latex-snippets.nvim",
+    ft = { "tex", "markdown" },
+    -- vimtex isn't required if using treesitter
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      config = function()
+        require("luasnip").config.setup({ enable_autosnippets = true })
+      end,
+    },
+    opts = {
+      use_treesitter = true,
+    },
+  },
+}
