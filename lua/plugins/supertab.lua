1e62df00a08c3d548b35bcb112c985254c29c884
Zhuofeng Wang <zhuofengwang2003@gmail.com>
2024-01-05 11:57:45 +0800

    Update <tab> and <cr> behavior.

--- lua/plugins/supertab.lua
+++ lua/plugins/supertab.lua
@@ -24,12 +24,8 @@ return {
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-            -- cmp.select_next_item()
             cmp.confirm({ select = true })
-          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-          -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_jumpable() then
+          elseif luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
           elseif has_words_before() then
             cmp.complete()
@@ -39,13 +35,24 @@ return {
         end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.select_prev_item()
+            cmp.confirm({
+              behavior = cmp.ConfirmBehavior.Replace,
+              select = true,
+            })
           elseif luasnip.jumpable(-1) then
             luasnip.jump(-1)
           else
             fallback()
           end
         end, { "i", "s" }),
+        ["<CR>"] = function(fallback)
+          cmp.abort()
+          fallback()
+        end,
+        ["<S-CR>"] = function(fallback)
+          cmp.abort()
+          fallback()
+        end,
       })
     end,
   },
