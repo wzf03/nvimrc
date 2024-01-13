89b16d4fd0c605a7dc87de169d0d2da4e38829fa
Zhuofeng Wang <zhuofengwang2003@gmail.com>
2024-01-13 13:06:17 +0800

    Fix: map <kEnter> to <Enter> to fix problems on some laptops

--- init.lua
+++ init.lua
@@ -2,3 +2,8 @@ local opt = vim.opt
 
 opt.clipboard = ""
 opt.spelllang = {}
+
+-- solve problems on Redmibook Pro 15
+vim.api.nvim_set_keymap("", "<kEnter>", "<Enter>", {})
+vim.api.nvim_set_keymap("i", "<kEnter>", "<Enter>", {})
+vim.api.nvim_set_keymap("c", "<kEnter>", "<Enter>", {})
