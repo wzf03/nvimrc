5af404ded16c9ce403fdc279440b6d2b9d4927d0
Zhuofeng Wang <zhuofengwang2003@gmail.com>
2025-02-26 09:53:00 +0800

    Update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,13 +13,70 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc",
+        "html", "css", "markdown", "fish"
+      },
+    },
+  },
+
+  {
+    "lervag/vimtex",
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    keys = {
+      { "<leader>tv", "<cmd>VimtexView<cr>",    desc = "Vimtex Preview" },
+      { "<leader>tc", "<cmd>VimtexCompile<cr>", desc = "Vimtex Compile" }
+    },
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = "skim"
+      vim.g.vimtex_syntax_enabled = 1
+      vim.o.conceallevel = 2
+    end
+  },
+
+  {
+    "L3MON4D3/LuaSnip",
+    opts = {
+      enable_autosnippets = true
+    },
+    build = "make install_jsregexp",
+  },
+
+  {
+    "wzf03/luasnip-latex-snippets.nvim",
+    branch = "evesdropper",
+    ft = "tex",
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "lervag/vimtex",
+    },
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local cmp = require("cmp")
+      local conf = require("nvchad.configs.cmp")
+
+      local mymappings = {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if require("luasnip").expand_or_jumpable() then
+            require("luasnip").expand_or_jump()
+          elseif cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }
+      conf.mapping = vim.tbl_deep_extend("force", conf.mapping, mymappings)
+      return conf
+    end,
+  },
+
 }
