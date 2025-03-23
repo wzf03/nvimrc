6b4c857fc974004b3bda7bfa5f3fee5b22fbe437
Zhuofeng Wang <zhuofengwang2003@gmail.com>
2025-03-23 10:57:20 +0800

    update snippets

--- lua/options.lua
+++ lua/options.lua
@@ -5,3 +5,5 @@ require "nvchad.options"
 local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 o.clipboard = ""
+
+vim.g.lua_snippets_path = vim.fn.stdpath "config" .. "/lua/lua_snippets"
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -42,19 +42,14 @@ return {
   {
     "L3MON4D3/LuaSnip",
     opts = {
-      enable_autosnippets = true
+      enable_autosnippets = true,
+      store_selection_keys = "<Tab>"
     },
     build = "make install_jsregexp",
   },
 
   {
-    "wzf03/luasnip-latex-snippets.nvim",
-    branch = "evesdropper",
-    ft = "tex",
-    dependencies = {
-      "L3MON4D3/LuaSnip",
-      "lervag/vimtex",
-    },
+    "lervag/vimtex",
   },
 
   {
@@ -73,10 +68,24 @@ return {
             fallback()
           end
         end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if require("luasnip").jumpable(-1) then
+            require("luasnip").jump(-1)
+          elseif cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" })
       }
       conf.mapping = vim.tbl_deep_extend("force", conf.mapping, mymappings)
       return conf
     end,
   },
-
+  {
+    "windwp/nvim-autopairs",
+    opts = {
+      disable_filetype = { "TelescopePrompt", "vim", "tex" },
+    },
+  }
 }
